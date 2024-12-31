c4bff5ccfe5de6a1bd60356fc25ad40a8ea58600
avi <avrikaufman@gmail.com>
2024-12-31 17:06:26 +0200

    Connect server to db

--- server/app.py
+++ server/app.py
@@ -1,6 +1,15 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, jsonify
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv, dotenv_values
+import os
 
+load_dotenv('..')
 app = Flask(__name__, static_folder='../client/build', static_url_path='')
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+db = MySQL(app=app)
 
 # Serve React App
 @app.route('/')
@@ -11,6 +20,15 @@ def serve():
 @app.route('/<path:path>')
 def static_files(path):
     return send_from_directory(app.static_folder, path)
+
+# Serve get all locations end points
+@app.route('/api/location', methods=['GET'])
+def get_location():
+    cur = db.connection.cursor()
+    cur.execute('SELECT * FROM location')
+    data = cur.fetchall()
+    cur.close()
+    return jsonify(data)
 
 
 if __name__ == '__main__':
