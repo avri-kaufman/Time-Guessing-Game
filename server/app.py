87db52a40ab2e3b0f4f5434ef3361d48a65cd3dc
avi <avrikaufman@gmail.com>
2024-12-31 17:40:52 +0200

    minimal server functionality added. we need to add err handaling

--- server/app.py
+++ server/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory, jsonify, request
 from flask_mysqldb import MySQL
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 import os
+import datetime
 
 load_dotenv('..')
 app = Flask(__name__, static_folder='../client/build', static_url_path='')
@@ -21,15 +22,41 @@ def serve():
 def static_files(path):
     return send_from_directory(app.static_folder, path)
 
-# Serve get all locations end points
-@app.route('/api/location', methods=['GET'])
+# Serve get all locations end point
+@app.route('/api/locations', methods=['GET'])
 def get_location():
     cur = db.connection.cursor()
     cur.execute('SELECT * FROM location')
     data = cur.fetchall()
     cur.close()
-    return jsonify(data)
+    locations = [{'id': row[0], 'name': row[1]} for row in data]
+    return jsonify(locations)
 
+# Serve submit guess end point
+@app.route('/api/guesses', methods=['POST'])
+def create_guess():
+    cur = db.connection.cursor()
+    user_id = request.json['user_id']
+    location_id = request.json['location_id']
+    time_guessed = request.json['time_guess']
+    actual_time = datetime.datetime.now()
+    cur.execute('INSERT INTO guess (user_id, location_id, time_guessed, actual_time) VALUES (%s, %s, %s, %s)', (user_id, location_id, time_guessed, actual_time))
+    db.connection.commit()
+    cur.close()
+    return jsonify({'message': 'Guess added successfully'})
+
+# Serve get all user guesses    
+@app.route('/api/guesses/<int:id>', methods=['GET'])
+def get_user_guesses(id):
+    cur = db.connection.cursor()
+    cur.execute('SELECT * FROM guess WHERE user_id= %s', (id,))
+    data = cur.fetchall()
+    cur.close()
+    guesses = [
+        {'id': row[0], 'user_id': row[1], 'location_id': row[2], 'time_guessed': row[3], 'actual_time': row[4]}
+        for row in data
+    ]
+    return jsonify(guesses)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
